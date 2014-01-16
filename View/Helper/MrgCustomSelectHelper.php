d41b5ed39ed8e2f6cb93788a997aa03e44e99659
styks1987 <mike@saymikeo.com>
2014-01-16 09:44:28 -0500

    slider, locations, as plugins

---
+++ View/Helper/MrgCustomSelectHelper.php
@@ -0,0 +1,17 @@
+<?php
+	App::uses('AppHelper', 'View/Helper');
+	class MrgCustomSelectHelper extends AppHelper{
+		var $helpers = ['Js'=>['MrgGoogleMap.MrgJqueryEngineHelper'],'Html'];
+		public function __construct(View $view, $settings = array()) {
+			parent::__construct($view, $settings);
+			// Load a custom select box
+			echo
+				$this->Html->css('MrgCustomSelect.jquery.plugins.selectBoxIt', array('inline'=>false)).
+				$this->Html->script('MrgCustomSelect.jquery.selectBoxIt.js', array('inline'=>false));
+		}
+
+		public function init($element){
+			return $this->Js->buffer($this->Js->get($element)->selectBoxIt());
+		}
+	}
+?>
