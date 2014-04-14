5bc07891f17c3b1b06c01123e63f0d19ffc076fb
styks1987 <mike@saymikeo.com>
2014-04-14 10:37:43 -0400

    add placeholder to custom select helper

--- View/Helper/MrgCustomSelectHelper.php
+++ View/Helper/MrgCustomSelectHelper.php
@@ -4,14 +4,20 @@
 		var $helpers = ['Js','Html'];
 		public function __construct(View $view, $settings = array()) {
 			parent::__construct($view, $settings);
-			// Load a custom select box
-			echo
-				$this->Html->css('MrgCustomSelect.jquery.plugins.selectBoxIt', array('inline'=>false)).
-				$this->Html->script('MrgCustomSelect.jquery.selectBoxIt.js', array('inline'=>false));
+
 		}
 
 		public function init($element, $options = []){
+			echo
+				$this->Html->css('MrgCustomSelect.jquery.plugins.selectBoxIt', array('inline'=>false)).
+				$this->Html->script('MrgCustomSelect.jquery.selectBoxIt.js', array('inline'=>false));
 			return $this->Js->buffer($this->Js->get($element)->selectBoxIt($options));
+		}
+
+		public function placeholder($inputs = ['input, textarea'], $otpions = []){
+			echo
+				$this->Html->script('MrgCustomSelect.jquery.placeholder.js', array('inline'=>false));
+			return $this->Js->buffer($this->Js->get(implode(',', $inputs))->placeholder($options));
 		}
 	}
 ?>
